a2d82620155926fc56b018e6db57f909d2274429
Slava Ivantsou <iwawiac@gmail.com>
2024-03-20 23:28:25 +0100

    jenkins

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Select user and date to see the journal Jenkins working</h1>
+      <h1>Select user and date to see the journal</h1>
       <UserForm
         selectedUser={selectedUser}
         selectedDate={selectedDate}
