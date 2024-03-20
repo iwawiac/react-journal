2d891931053a3e214a34969bb940b796d4bfafac
Slava Ivantsou <iwawiac@gmail.com>
2024-03-20 23:30:22 +0100

    js

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,6 @@ function App() {
   return (
     
     <div className="container">
-      <h1>Jenkins deploy</h1>
       <h1>Select user and date to see the journal</h1>
       <UserForm
         selectedUser={selectedUser}
